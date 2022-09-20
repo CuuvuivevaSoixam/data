c84cb874e7af6956163f156764c13de457e520f4
CuuvuivevaSoixam <107055520+CuuvuivevaSoixam@users.noreply.github.com>
2022-09-20 09:55:00 +0700

    Add files via upload

---
+++ lad/main.js
@@ -0,0 +1,24 @@
+ function doimau1() {
+    const x = document.getElementById("box1").value
+    if (x<20) {
+        col1.style.background='blue'
+    }else if(x<20){
+        col1.style.background='back'
+    }else{
+        col1.style.background='red'
+        alert('de so be thoi broooo!!!!')
+    }
+}
+ function doimau2(){
+    var y= document.getElementById("nut1").style.background = 'red'
+    var z= document.getElementById("nut2").style.background = 'blue'
+    if (condition) {
+        
+    } else {
+        
+    }
+    
+ }
+ function doimau3(){
+    var y= document.getElementById("nut1").style.background = 'black'
+ }
